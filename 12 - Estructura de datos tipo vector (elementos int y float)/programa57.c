778067494c72e35172c0ac6e411e62bfb4a4cae0
ded2792 <ded27@live.com.ar>
2025-02-23 13:05:17 -0300

    Distintos programas de C

---
+++ 12 - Estructura de datos tipo vector (elementos int y float)/programa57.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int f;
+    int sueldos[5];
+    //Carga del vector
+    for (f=0; f<5; f++)
+    {
+        printf("Ingrese valor del sueldo:");
+        scanf("%i",&sueldos[f]);
+    }
+    printf("Listado de sueldos.");
+    printf("\n");
+    //impresion del vector
+    for(f=0; f<5; f++)
+    {
+        printf("%i",sueldos[f]);
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
